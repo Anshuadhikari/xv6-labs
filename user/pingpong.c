916d92f84a4bde6eceb92816af204e53b5c10306
Anshu Adhikari <anshu.adhikari10@gmail.com>
2024-09-08 02:39:53 -0400

    Modified xargs, renamed answers-util.txt, and other updates

--- user/pingpong.c
+++ user/pingpong.c
@@ -5,6 +5,26 @@
 int
 main(int argc, char *argv[])
 {
-  fprintf(1, "pingpong does not work\n");
-  exit(0);
+   int p[2];
+   int p2[2];
+   pipe(p);
+   pipe(p2);
+   char buffer[1];
+   int pid = fork();
+   write(p[1], &buffer, 1);
+   close(p[0]);
+   if (pid == 0) {
+     read(p[0], &buffer, 1);
+     close(p[1]);
+     printf("%d: received ping\n", getpid());
+     write(p2[1], &buffer, 1);
+     close(p2[0]);
+     exit(0);
+   } else {
+     read(p2[0], &buffer, 1);
+     close(p2[1]);
+     printf("%d: received pong\n", getpid());
+     exit(0);
+   }
+   exit(0);
 }
